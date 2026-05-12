a86c27e45cfe7f8ff440c6c363b119d6dbd325e1
Daniel King <damaki.gh@gmail.com>
2026-05-12 12:22:26 +0100

    common: Add Always_Terminates and format with GNATformat

--- common/src/adabee-time_units.ads
+++ common/src/adabee-time_units.ads
@@ -3,9 +3,9 @@
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
-package AdaBee.Time_Units with
-  Pure,
-  SPARK_Mode => On
+
+package AdaBee.Time_Units
+  with Pure, SPARK_Mode, Always_Terminates
 is
 
    Time_Unit : constant := 0.000_001; --  1 microsecond resolution
@@ -13,10 +13,9 @@ is
 
    type Time_Span is
      delta Time_Unit
-     range -Time_Unit * (2.0**(Time_Size - 1) - 1.0) ..
-            Time_Unit * (2.0**(Time_Size - 1) - 1.0)
-   with
-     Small => Time_Unit;
+     range -Time_Unit * (2.0 ** (Time_Size - 1) - 1.0)
+           .. Time_Unit * (2.0 ** (Time_Size - 1) - 1.0)
+   with Small => Time_Unit;
    --  Represents a length of real time duration in seconds.
 
    subtype Time is Time_Span range 0.0 .. Time_Span'Last;
